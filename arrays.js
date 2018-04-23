117a23c8d5d1f3422ef5be44df1f04fb30774432
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-23 20:24:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   return newArray
 }
 
-function destructivelyAddElementToBeginningofArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
  array.unshift(element)
   return array
 }
