f8cf2818d1a523e40234fe0028c2db71f342a7e6
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-23 19:51:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementtoBeginningOfArray() {}
+function addElementtoBeginningOfArray(array, element) {
+  var thisArray = array
+  thisArray.unshift(element)
+  return thisArray
+}
+
+function destructivelyAddElementToBeginningofArray(array, element) {
+
+}
