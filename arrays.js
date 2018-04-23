c761e6f685f320a8ed52932a7028b43ed5442123
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-23 20:21:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,12 @@ function removeElementFromBeginningOfArray(array) {
   return newArray 
 }
 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array 
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length  - 1)
+}
+
